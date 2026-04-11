777a2db8b689bf19fdd16748634393202d3ddb24
gmurr20 <greg.murray20@gmail.com>
2026-04-11 13:11:03 -0500

    Have app create tables

--- backend/app.py
+++ backend/app.py
@@ -34,5 +34,7 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
+    with app.app_context():
+        db.create_all()
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=True)
